a317dbbaa2582d9e9634f556018a5498dbc9570d
Jordan Kimsey <jordankimsey@gmail.com>
2022-05-31 12:38:22 -0400

    Added functionality to navbar and sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,23 +14,49 @@ import { useStateContext } from '../contexts/ContextProvider';
 const NavButton = ({title, customFunc, icon, color, dotColor}) => (
   <TooltipComponent content={title}
   position="BottomCenter">
-    <button type='button' onClick={customFunc} style={{color}} className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
-      <span style={{background: dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'>
+    <button type='button' onClick={() => customFunc()} style={{color}} className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
+      <span style={{background: dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'/>
         {icon}
-      </span>
     </button>
   </TooltipComponent>
 )
 
+
+
 const NavBar = () => {
-  const {activeMenu, setActiveMenu} = useStateContext();
+  const {activeMenu, setActiveMenu, isClicked, handleClick, screenSize, setScreenSize} = useStateContext();
+  
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+    
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+
+  }, []);
+
+  useEffect(() => {
+    if(screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
+    const handleActiveMenu = () => setActiveMenu(!activeMenu);
+ 
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
-      <NavButton title='Menu' customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color='blue' icon={<AiOutlineMenu/>} />
+
+      <NavButton title='Menu' customFunc={handleActiveMenu} color='blue' icon={<AiOutlineMenu/>} />
+      
       <div className='flex'>
         <NavButton title='Cart' customFunc={() => handleClick('cart')} color='blue' icon={<FiShoppingCart/>} />
          <NavButton title='Chat' dotColor='#03c9d7' customFunc={() => handleClick('chat')} color='blue' icon={<BsChatLeft/>} />
          <NavButton title='Notifications' customFunc={() => handleClick('notification')} color='blue' icon={<RiNotification3Line/>} />
+         
          <TooltipComponent
          content="Profile"
          position='BottomCenter'
@@ -41,6 +67,7 @@ const NavBar = () => {
             <img
               className='rounded-full w-8 h-8' 
               src={avatar}
+              alt='user-profile'
             />
             <p>
               <span className='text-gray-400 text-14'>Hi, </span> {' '}
@@ -48,11 +75,16 @@ const NavBar = () => {
             </p>
             <MdKeyboardArrowDown className='text-gray-400 text-14'/>
            </div>
-
          </TooltipComponent>
+      
+         {isClicked.cart && (<Cart />)}
+         {isClicked.chat && (<Chat />)}
+         {isClicked.notification && (<Notification />)}
+         {isClicked.userProfile && (<UserProfile />)}
+         
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -3,21 +3,24 @@ import React, {createContext, useContext, useState} from 'react';
 const StateContext = createContext();
 
 const initialState = {
-    chat: false,
-    cart: false,
-    userProfile: false,
-    notification: false,
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
 };
 
 export const ContextProvider = ({ children}) => {
-    
-    const [activeMenu, setActiveMenu] = useState(true)
+
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined)
+
+      const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
     
     return (
-        <StateContext.Provider
-        value={{ activeMenu, setActiveMenu,}}>
-            {children}
-        </StateContext.Provider>
+       <StateContext.Provider value={{ activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu}}>
+      {children}
+    </StateContext.Provider>
     );
 };
 
